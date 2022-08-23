3517e27ce879407abd88751da6c1714f7e954764
Avery Novick <averynov@yahoo.com>
2022-08-23 17:15:06 -0400

    footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <div className="footer-info">
+        <div className="f-name">
+          <p>Avery Novick</p>
+        </div>
+        <div className="f-contact">
+          <div className="f-info">
+            <p>averynov@yahoo.com</p>
+          </div>
+          <div className="f-info">
+            <p>(781) 290-8623</p>
+          </div>
+        </div>
+        <div className="socials" id="footer">
+          <a
+            href="https://www.linkedin.com/in/avery-novick/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25320.png" />
+          </a>
+          <a
+            href="https://github.com/anovick1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="https://cdn-icons-png.flaticon.com/512/1051/1051377.png" />
+          </a>
+          <a href="PUT RESUME HERE" target="_blank" rel="noopener noreferrer">
+            <img src="https://cdn-icons-png.flaticon.com/512/85/85047.png" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
